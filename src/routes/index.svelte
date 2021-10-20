2c90f24d8e3b3638cc8f58aeb4275ebf20cd7165
Tilo <tilo.codes@gmail.com>
2021-10-20 20:26:58 +0200

    Make svelte kit run with deno

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
 <script lang="ts">
 	import { Napolitan } from '../recipes/napolitan';
 	import type { Text } from '../recipes/recipe';
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
 import preprocess from 'svelte-preprocess';
+import adapter from 'svelte-adapter-deno';
+// import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,8 +9,10 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter({
+			out: 'build'
+		  })
+		// adapter: node()
 	}
 };
 
